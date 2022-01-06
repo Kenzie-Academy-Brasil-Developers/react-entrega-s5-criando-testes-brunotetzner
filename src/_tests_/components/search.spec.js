8106fdc9bf1cd667caae30507d7ede5ab1f3bd7e
Bruno Tetzner <brunoltetzner@gmail.com>
2022-01-06 15:17:00 -0300

    .

--- src/_tests_/components/search.spec.js
+++ src/_tests_/components/search.spec.js
@@ -15,6 +15,7 @@ import Providers from "../../providers/index";
 import Address from "../../components/Cep";
 import { findAllInRenderedTree } from "react-dom/test-utils";
 
+const apiMock = new MockAdapter(api);
 describe("search cep", () => {
   it("should be search for a cep", async () => {
     render(
@@ -34,31 +35,7 @@ describe("search cep", () => {
   });
 
   it("should be see informations by cep", async () => {
-    const cepField = screen.getByPlaceholderText("Insira o CEP");
-    const buttonElement = screen.getByText("Buscar pelo CEP");
-    fireEvent.change(cepField, { target: { value: "23545037" } });
-    fireEvent.click(buttonElement);
-    jest.mock("../../providers/CepProvider", () => {
-      return {
-        LocateCepProvider: () => ({
-          handleSearch: jest.fn(),
-          setCeps: {
-            bairro: "Sepetiba",
-            cidade: "Rio de Janeiro",
-            logradouro: "Rua 1 (Acesso Est Sepetiba 4718)",
-            estado_info: {
-              area_km2: "43.781,566",
-              codigo_ibge: "33",
-              nome: "Rio de Janeiro",
-            },
-            cep: "23545037",
-            cidade_info: { area_km2: "1200,179", codigo_ibge: "3304557" },
-            estado: "RJ",
-          },
-        }),
-      };
-    });
-
+    apiMock.onGet("23545037").replyOnce(200, []);
     render(
       <Providers>
         <Address />
