30782f6027f5e694bbe25b2bad17f26876e792f1
Bruno Tetzner <brunoltetzner@gmail.com>
2022-01-07 12:01:24 -0300

    finalizado

--- cypress/integration/adress-E2E.spec.js
+++ cypress/integration/adress-E2E.spec.js
@@ -1,9 +1,8 @@
-const cypress = require("cypress");
-
 context("Search ceps", () => {
-  it("Should be search for ceps", () => {});
-  cy.visit("http://localhost:3000");
-  cy.viewport(1440, 900);
-  cy.get("input[Insira o CEP=Insira o CEP]").type("23545037");
-  cy.contains("Buscar pelo CEP").click();
+  it("Should be search for ceps - test 1", () => {
+    cy.visit("http://localhost:3000");
+    cy.viewport(1440, 900);
+    cy.get("input").type("23545037");
+    cy.contains("Buscar pelo CEP").click();
+  });
 });
--- src/_tests_/components/search.spec.js
+++ src/_tests_/components/search.spec.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import App from "../../App";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import api from "../../services/index";
